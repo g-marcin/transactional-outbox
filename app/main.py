70eea78bf9c78a7c9cfe8122cdf479beb07a8086
Marcin Grzmil <marcin.grzmil@dynatrace.com>
2026-05-23 13:50:06 +0200

    add descriptive comments to main.py

--- app/main.py
+++ app/main.py
@@ -3,13 +3,15 @@ from fastapi import FastAPI
 from api import health, orders
 from worker import OutboxWorker
 
+# __CLIENT__
 app = FastAPI()
 app.include_router(orders.router)
 app.include_router(health.router)
 
+#__OUTBOX_POLLING__
 _worker = OutboxWorker()
 
-
+# FastAPI lifespan hook
 @app.on_event("startup")
 def startup_event() -> None:
     _worker.start()
